64bd5a1dc4c19d05ce4b661d328c1ca865d06e91
rbradford-dev <276454226+rbradford-dev@users.noreply.github.com>
2026-07-11 15:16:02 -0400

    Add stub: src/etl/load_matches.py

---
+++ src/etl/load_matches.py
@@ -0,0 +1,6 @@
+"""ETL: load international match results into a unified table."""
+
+
+def load_matches():
+    """Return a DataFrame of matches: date, home, away, neutral, home_score, away_score, competition."""
+    raise NotImplementedError
